46feada259a5f10df9e69f6d0eddfc271d6fee99
Mustkeem Baraskar <mustkeembaraskar@gmail.com>
2024-09-30 00:34:07 +0530

    configration

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,16 +1,18 @@
+require('dotenv').config();
 const { initializeApp } = require('@firebase/app');
 const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } = require('@firebase/auth');
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAb4XedWTkRzw3kZPnzp9Y6mFGzoaYckxI",
-  authDomain: "insta-bank.firebaseapp.com",
-  projectId: "insta-bank",
-  storageBucket: "insta-bank.appspot.com",
-  messagingSenderId: "677996773923",
-  appId: "1:677996773923:web:34755c0bd275c021cba796",
-  measurementId: "G-BJ5VQXBXTL"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
+
 
 
 const app = initializeApp(firebaseConfig);
