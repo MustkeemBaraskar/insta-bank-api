59592884368784bcea7f2faa0f0b70b5a7f729a5
Mustkeem Baraskar <mustkeembaraskar@gmail.com>
2024-10-07 01:15:48 +0530

    fixed all issues

--- src/config/pgdatabase.js
+++ src/config/pgdatabase.js
@@ -1,35 +1,21 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false
   }
-})
+});
 
-pool.connect((err)=>{
-    if(err) throw err
-    console.log("connection to postgree is successfull");
-})
+pool.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the PostgreSQL database:', err.stack);
+  } else {
+    console.log('Connected to PostgreSQL database successfully');
+  }
+});
+
 
 
 module.exports = pool;
-
-const createUsersTableQuery = `
-  CREATE TABLE IF NOT EXISTS users (
-    uid SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    phone_number VARCHAR(20) UNIQUE NOT NULL,
-    branch VARCHAR(100) NOT NULL,
-    account_balance NUMERIC(12, 2) DEFAULT 0 NOT NULL,
-    password TEXT NOT NULL,
-    upi_password TEXT NOT NULL
-  );
-`;
-
-pool.query(createUsersTableQuery)
-  .then(() => console.log("Users table created successfully"))
-  .catch((err) => console.error("Error creating users table", err));
